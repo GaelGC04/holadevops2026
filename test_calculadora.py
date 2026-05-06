c7d0aae8863373cacd290ef7d67eb7abcf9c749c
GaelGC04 <garciajorgecf@gmail.com>
2026-05-05 23:41:43 -0600

    añadiendo pruebas

---
+++ test_calculadora.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest import TestCase
+from calculadora import Calculadora
+
+
+class TestCalculadora(TestCase):
+    def test_sumar_dos_mas_dos(self):
+        calc = Calculadora()
+        resultado = calc.sumar(2, 2)
+        self.assertEqual(4, resultado)
+
+
+if __name__ == '__main__':
+    unittest.main()
